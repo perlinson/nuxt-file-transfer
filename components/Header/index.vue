6dd8ec9ffa0583c595a2830dceef4aeca7ce4910
perlinson <perlinson_2008@163.com>
2021-10-23 13:03:31 +0800

    实现基本信息的获取

---
+++ components/Header/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-contaner >
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">
+        <nuxt-link class="nav-link active" aria-current="page" to="/">
+          Home
+        </nuxt-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <nuxt-link
+          class="nav-link active"
+          aria-current="page"
+          to="/fileList"
+        >
+          File List
+        </nuxt-link>
+      </el-menu-item>
+    </el-menu>
+  </el-contaner>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: '1',
+    }
+  },
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated // it check if user isAuthenticated
+    },
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout() // this method will logout the user and make token to false on the local storage of the user browser
+    },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath)
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/fileList.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-container direction="vertical">
+    <el-row>
+      <el-col :span="24"
+        ><div class="grid-content bg-purple-dark">FILE lIST</div></el-col
+      >
+    </el-row>
+
+    <el-table :data="tableData" title="FILE LSIT" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+  </el-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const res = await $axios.$get('/api/file/list')
+    return { tableData: res.data }
+  },
+
+  data() {
+    return {}
+  },
+}
+</script>
